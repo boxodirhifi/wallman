4ac4022856bc9a4c1a80ef25037de065652de606
Yuta <boxodir71777@gmail.com>
2026-08-05 14:28:39 +0500

    feat: cache current wallpaper

--- src/commands/set.rs
+++ src/commands/set.rs
@@ -1,10 +1,23 @@
-use std::path::PathBuf;
+use directories::ProjectDirs;
+use std::{fs, path::PathBuf};
 
 pub fn run(image: PathBuf) {
     if !image.exists() {
         eprintln!("Error: '{}' does not exist.", image.display());
         std::process::exit(1);
     }
+
+    let project_dirs =
+    ProjectDirs::from("", "", "wallman").expect("Failed to determine cache directory");
+
+    let cache_dir = project_dirs.cache_dir();
+
+    fs::create_dir_all(cache_dir).expect("Failed to create cache directory");
+
+    let cached_wallpaper = cache_dir.join("current");
+
+    fs::copy(&image, &cached_wallpaper)
+    .expect("Failed to copy wallpaper into cache");
 
     println!("Setting wallpaper: {}", image.display());
 }
